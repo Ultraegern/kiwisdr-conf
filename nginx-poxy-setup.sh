ca24357621e2d141df72a75aeab6091d86ff6bba
Egern <155380537+Ultraegern@users.noreply.github.com>
2025-09-15 13:43:32 +0200

    Update nginx-poxy-setup.sh

--- nginx-poxy-setup.sh
+++ nginx-poxy-setup.sh
@@ -1,5 +1,14 @@
 !#/bin/bash
 
 sudo apt update
-sudo apt upgrade -y
-sudo reboot
+sudo apt install curl gnupg2 ca-certificates lsb-release debian-archive-keyring
+
+#configure repos
+curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] http://nginx.org/packages/debian `lsb_release -cs` nginx" | sudo tee /etc/apt/sources.list.d/nginx.list
+echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" | sudo tee /etc/apt/preferences.d/99nginx
+
+sudo apt update
+sudo apt install nginx
+
+
