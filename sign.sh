6eb314ca56e6fece3bf01b42a6e287d5b9a77336
Egern <155380537+Ultraegern@users.noreply.github.com>
2025-09-17 14:57:33 +0200

    Update sign.sh

--- sign.sh
+++ sign.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 gpg --batch --yes --armor --detach-sign --output nginx-setup.sh.asc nginx-setup.sh
+gpg --batch --yes --armor --detach-sign --output kiwiclient-setup.sh.asc kiwiclient-setup.sh
