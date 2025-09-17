3299c16abd40f516fbe95b92a889b7a0aa4f4cfb
Egern <155380537+Ultraegern@users.noreply.github.com>
2025-09-17 16:42:12 +0200

    seperate files v3

--- sign.sh
+++ sign.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-gpg --batch --yes --armor --detach-sign --output nginx-setup.sh.asc nginx-setup.sh
+gpg --batch --yes --armor --detach-sign --output cert/renew-cert.service.asc cert/renew-cert.service
+gpg --batch --yes --armor --detach-sign --output cert/renew-cert.timer.asc cert/renew-cert.timer
+gpg --batch --yes --armor --detach-sign --output cert/renew-cert.sh.asc cert/renew-cert.sh
+
+gpg --batch --yes --armor --detach-sign --output html/502.html.asc html/502.html
+gpg --batch --yes --armor --detach-sign --output html/recorder.html.asc html/recorder.html
+
+gpg --batch --yes --armor --detach-sign --output nginx/nginx-setup.sh.asc nginx/nginx-setup.sh
+gpg --batch --yes --armor --detach-sign --output nginx/nginx.conf.asc nginx/nginx.conf
+
 gpg --batch --yes --armor --detach-sign --output kiwiclient-setup.sh.asc kiwiclient-setup.sh
