8e3cce372858e2b53dbb6dfc242c700faa33a8ae
Egern <155380537+Ultraegern@users.noreply.github.com>
2025-09-18 12:19:17 +0200

    Improve signature verification in setup.sh
    Renamed PGP_FINGERPRINT to PGP_KEY_FINGERPRINT for clarity. Enhanced the verify_signature function to use gpg --fingerprint and provide more detailed output. Added execution of nginx/nginx-setup.sh after verification and cleanup of /tmp/kiwisdr-conf.

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-PGP_FINGERPRINT="3CB2F77A8047BEDC"
+PGP_KEY_FINGERPRINT="3CB2F77A8047BEDC"
 
 verify_signature() {
   local file="$1"
@@ -10,20 +10,22 @@ verify_signature() {
 
   echo "⬜ Verifying the signature of $file..."
 
-  # Perform the signature verification
-  local signature=$(gpg --verify "$file_asc" "$file" 2>&1)
-
+  local gpg_output=$(gpg --verify --keyring=- --fingerprint "$file_asc" "$file" 2>&1)
   # Check the verification result. This is a basic check;
   # more robust checks might involve parsing the output of gpg --verify and
   # checking specific error codes.
-  if grep -q "gpg: Good signature from" <<< "$signature" ; then
-    echo "✅ Signature verified successfully."
+  if grep -q "gpg: Good signature from " <<< "$gpg_output"; then
+    echo "✅ Signature verified successfully for $file."
     return 0 # Success
   else
-    echo "❌ Error: Signature verification failed."
-    echo "--- Verification Output ---"
-    echo "$signature"
-    echo "--- End Verification Output ---"
+    echo "❌ Error: Signature verification failed for $file."
+    echo "ℹ️ --- Verification Output ---"
+    echo "$gpg_output"
+    echo "ℹ️ --- End Verification Output ---"
     return 1 # Failure
   fi
 }
+
+verify_signature nginx/nginx-setup.sh && ./nginx/nginx-setup.sh
+
+rm -R /tmp/kiwisdr-conf
