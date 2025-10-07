9398c2b9bd51d3a5db50d3e06421ea254d5cb896
Egern <155380537+Ultraegern@users.noreply.github.com>
2025-10-07 12:02:40 +0200

    Update sign.sh

--- sign.sh
+++ sign.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
-gpg --batch --yes --armor --detach-sign --output cert/renew-cert.service.asc cert/renew-cert.service
-gpg --batch --yes --armor --detach-sign --output cert/renew-cert.timer.asc cert/renew-cert.timer
-gpg --batch --yes --armor --detach-sign --output cert/renew-cert.sh.asc cert/renew-cert.sh
+sign () {
+    local file="$1"
+    gpg --batch --yes --armor --detach-sign --output "${file}.asc" "$file"
+    echo "✅ Signed $file"
+}
 
-gpg --batch --yes --armor --detach-sign --output html/502.html.asc html/502.html
-gpg --batch --yes --armor --detach-sign --output html/recorder.html.asc html/recorder.html
-gpg --batch --yes --armor --detach-sign --output html/filebrowser.html.asc html/filebrowser.html
-gpg --batch --yes --armor --detach-sign --output html/help.html.asc html/help.html
-gpg --batch --yes --armor --detach-sign --output html/stylesheet.css.asc html/stylesheet.css
+sign cert/renew-cert.service
+sign cert/renew-cert.timer
+sign cert/renew-cert.sh
 
-gpg --batch --yes --armor --detach-sign --output nginx/nginx-setup.sh.asc nginx/nginx-setup.sh
-gpg --batch --yes --armor --detach-sign --output nginx/nginx.conf.asc nginx/nginx.conf
+sign html/502.html
+sign html/recorder.html
+sign html/filebrowser.html
+sign html/help.html
+sign html/stylesheet.css
 
-gpg --batch --yes --armor --detach-sign --output recorder/kiwiclient/kiwiclient-setup.sh.asc recorder/kiwiclient/kiwiclient-setup.sh
-gpg --batch --yes --armor --detach-sign --output recorder/webui-backend/backend-setup.sh.asc recorder/webui-backend/backend-setup.sh
-gpg --batch --yes --armor --detach-sign --output recorder/webui-backend/backend.service.asc recorder/webui-backend/backend.service
-gpg --batch --yes --armor --detach-sign --output recorder/webui-backend/backend.py.asc recorder/webui-backend/backend.py
+sign nginx/nginx-setup.sh
+sign nginx/nginx.conf
 
-gpg --batch --yes --armor --detach-sign --output setup.sh.asc setup.sh
+sign recorder/kiwiclient/kiwiclient-setup.sh
+sign recorder/webui-backend/backend-setup.sh
+sign recorder/webui-backend/backend.service
+sign recorder/webui-backend/backend.py
+
+sign setup.sh
