01cac1bdcbe52e6196eda5ea723bc66989262b73
Egern <155380537+Ultraegern@users.noreply.github.com>
2025-10-11 15:28:20 +0200

    status is now plain text instead of json + start of start_recording

--- recorder/backend/src/main.rs
+++ recorder/backend/src/main.rs
@@ -1,14 +1,16 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
 use serde_json::json;
+use chrono;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let port: u16 = 5004;
+    
     println!("Starting server on port {}", port);
-
     HttpServer::new(|| {
         App::new()
             .service(status)
+            .service(start_recorder)
     })
     .bind(("0.0.0.0", port))?
     .run()
@@ -17,7 +19,17 @@ async fn main() -> std::io::Result<()> {
 
 #[get("/api/status")]
 async fn status() -> impl Responder {
+    HttpResponse::Ok().body(
+        "Api is Online"
+    )
+}
+
+#[post("/api/recorder/start")]
+async fn start_recorder() -> impl Responder {
+
+    let start_time = chrono::Utc::now().to_rfc3339();
     HttpResponse::Ok().json(json!({ 
-        "status": "Api is Online" 
+        "recording": true, 
+        "started_at": start_time
     }))
 }
